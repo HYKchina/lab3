8298c934618856bcef466a8dd7253c2554609665
HYKchina <h18003510118@163.com>
2025-05-07 23:52:38 +0800

    class Client main method

--- Client.java
+++ Client.java
@@ -1,8 +1,25 @@
 public class Client {
+
     // The host address of the server 
     private static final String SERVER_HOST = "localhost";
     // The port number of the server 
     private static final int SERVER_PORT = 9090;
+
+    // Main method: Entry point of the client program
+    public static void main(String[] args) {
+        // Define the list of client request files to be sent
+        String[] filePaths = {
+            "client_1.txt",
+            "client_2.txt",
+            "client_3.txt",
+            "client_4.txt",
+            "client_5.txt",
+            "client_6.txt",
+            "client_7.txt",
+            "client_8.txt",
+            "client_9.txt",
+            "client_10.txt"
+        };
 
 
 }
