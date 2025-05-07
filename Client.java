f8e9a82a3b770bcdcf19ee14cbc924e82f715d9d
HYKchina <h18003510118@163.com>
2025-05-07 23:51:34 +0800

    class Client variable settings

--- Client.java
+++ Client.java
@@ -1,3 +1,8 @@
 public class Client {
+    // The host address of the server 
+    private static final String SERVER_HOST = "localhost";
+    // The port number of the server 
+    private static final int SERVER_PORT = 9090;
+
 
 }
