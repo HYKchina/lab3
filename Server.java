c2aee3a903310a7ba4434d5daa124f389a416a42
HYKchina <h18003510118@163.com>
2025-05-07 20:35:02 +0800

    comment

--- Server.java
+++ Server.java
@@ -3,9 +3,10 @@ import java.net.*;
 import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-public class Java {
+public class Server {
+    //The time interval (in seconds) at which the server will print a summary of its operations.
     private static final int PORT = 9090;
-
+     //The server listens for the port number of the incoming client connection.
     private static final int TIMEOUT_SECONDS = 10;
     
     private final List<Tuple> tupleSpace = new ArrayList<>();
